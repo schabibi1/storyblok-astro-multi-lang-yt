987a7005abf60a5ed153e8028f02e76b6e4b8f6b
schabibi1 <arisagse@gmail.com>
2024-07-31 16:04:53 +0200

    add: deployment configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,36 @@
-import { defineConfig } from 'astro/config'
-import storyblok from '@storyblok/astro'
-import { loadEnv } from 'vite'
-import tailwind from '@astrojs/tailwind'
-import basicSsl from '@vitejs/plugin-basic-ssl'
-const env = loadEnv('', process.cwd(), 'STORYBLOK')
+import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import tailwind from '@astrojs/tailwind';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import vercel from "@astrojs/vercel/serverless";
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      apiOptions: {
-        region: '',
-      },
-      bridge: {
-        customParent: 'https://app.storyblok.com',
-      },
-      components: {
-        page: 'storyblok/Page',
-        feature: 'storyblok/Feature',
-        grid: 'storyblok/Grid',
-        teaser: 'storyblok/Teaser',
-        config: 'storyblok/Config',
-        hero: 'storyblok/Hero',
-        'popular-articles': 'storyblok/PopularArticles',
-        'all-articles': 'storyblok/AllArticles',
-        article: 'storyblok/Article'
-      },
-    }),
-    tailwind(),
-  ],
-  vite: {
-    plugins: [basicSsl()],
-    server: {
-      https: true,
-    },
-  },
-})
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    bridge: env.STORYBLOK_IS_PREVIEW === 'yes',
+    components: {
+      page: 'storyblok/Page',
+      feature: 'storyblok/Feature',
+      grid: 'storyblok/Grid',
+      teaser: 'storyblok/Teaser',
+      hero: 'storyblok/Hero',
+      config: 'storyblok/Config',
+      'popular-articles': 'storyblok/PopularArticles',
+      'all-articles': 'storyblok/AllArticles',
+      article: 'storyblok/Article'
+    }
+  }), tailwind()],
+  output: env.STORYBLOK_IS_PREVIEW === 'yes' ? 'server' : 'static',
+  ...(env.STORYBLOK_ENV === 'development' && {
+    vite: {
+      plugins: [basicSsl()],
+      server: {
+        https: true
+      }
+    }
+  }),
+  adapter: vercel()
+});
